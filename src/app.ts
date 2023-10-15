0cd2b7acd4944bc587256c8e2da6f6979099c38b
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-10-15 23:35:40 +0200

    task status

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,52 @@ interface Task {
     completed: boolean;
 }
 
-const tasks: Task[] = [{name: "ccc", completed: false}]
+const tasks: Task[] = [{name: "ccc", completed: false}, {name: "cabv", completed: true}]
 
 function render() {
     ul.innerText = ""
-    tasks.forEach((task) => {
+    tasks.forEach((task, id: number) => {
         const li = document.createElement("li");
         li.innerText = task.name
+        li.setAttribute("index", `${id}`)
         ul.appendChild(li)
+
+        const taskButton: HTMLButtonElement = document.createElement("button");
+        taskButton.classList.add("task-button")
+        taskButton.setAttribute("index", `${id}`)
+        li.appendChild(taskButton);
+
+        taskButton.innerText = task.completed ? "complete" : "uncomplete";
+        taskButton.addEventListener("click", (e) => {
+            e.preventDefault()
+            if(task.completed === true) {
+                taskButton.innerText = "uncomplete"
+                task.completed = false
+            } else {
+                taskButton.innerText = "complete"
+                task.completed = true
+            }
+        })
+
     });
 }
 
-button.addEventListener("click", function addTask(e) {
+function addTask(e: any) {
     e.preventDefault();
     tasks.push({name: input.value, completed: false})
     render()
-})
+}
+
+
+
+
+
+
+
+button.addEventListener("click", addTask) 
+   
+
+
 
 
 render()
